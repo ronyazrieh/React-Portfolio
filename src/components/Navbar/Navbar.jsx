6b5f1aca0f148ad654651520ce2fad5ef5e69a2c
Rony Azrieh <94086447+ronyazrieh@users.noreply.github.com>
2024-08-13 01:35:37 -0400

    Responsive Navbar for mobile devices

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,14 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 
 import styles from "./Navbar.module.css"
+import { getImageUrl } from "../../utils"
 
 export const Navbar = () => {
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    const handleCheckboxChange = () => {
+        setMenuOpen(!menuOpen);
+    };
+
     return (
         <nav className={styles.navbar}>
             <div />
             <div className={styles.nav}>
                 <a className={styles.title} href="/" >Rony Azrieh</a>
-                <ul className={styles.menu}>
+                <div className={styles.drop}>
+                <label className={styles.container}>
+                <input 
+                            type="checkbox" 
+                            checked={menuOpen}
+                            onChange={handleCheckboxChange}
+                        />
+                    <div className={styles.checkmark}>
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                    </div>
+                </label>
+                <ul className={`${styles.menu} ${menuOpen && styles.menuOpen}`} onClick={() => setMenuOpen(false)}  >
                     <li>
                         <a href="#about">About</a>
                     </li>
@@ -22,6 +42,7 @@ export const Navbar = () => {
                         <a href="#Contact">Contact</a>
                     </li>
                 </ul>
+                </div>
             </div>
         </nav>
     )
